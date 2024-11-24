4eac7de4257662a163dfc8e053349a0d2a33b873
iankorf <ifkorf@gmail.com>
2024-11-23 16:01:16 -0800

    up

--- alignment_client.py
+++ alignment_client.py
@@ -4,7 +4,6 @@ import sqlite3
 parser = argparse.ArgumentParser(description='aligns sequences, talks to db')
 parser.add_argument('--database', default='intronomicon.db',
 	help='database name [%(default)s]')
-#parser.add_argument('--min
 arg = parser.parse_args()
 
 # connect to db
@@ -17,20 +16,9 @@ arg = parser.parse_args()
 	# push intron data to db
 
 
-conn = sqlite3.connect(arg.database)
-cursor = conn.cursor()
+con = sqlite3.connect(arg.database)
+cur = con.cursor()
 
-cursor.execute('SELECT * from runs ORDER BY bases')
-for rid, eid, nts, seqs, filesize in cursor.fetchall():
-	if seqs != 0:
-		print(nts//seqs)
-
-"""
-CREATE TABLE runs(
-        run_ID TEXT PRIMARY KEY,
-        exp_ID TEXT,
-        bases INTEGER,
-        spots INTEGER,
-        file_size INTEGER,
-        FOREIGN KEY (exp_ID) REFERENCES experiment (exp_ID)
-"""
+cur.execute('SELECT * from runs ORDER BY bases')
+for x in cursor.fetchall():
+	pass
