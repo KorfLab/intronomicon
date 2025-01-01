126e72a1e99311ee1740426458fbcd660e96fd23
iankorf <ifkorf@gmail.com>
2025-01-01 15:19:51 -0800

    up

--- bakeoff/compare-alignments.py
+++ bakeoff/compare-alignments.py
@@ -7,12 +7,12 @@ from ftx import FTX
 
 parser = argparse.ArgumentParser(description='alignment evaluator')
 parser.add_argument('files', nargs='+', help='run-aligner.py output files')
-parser.add_argument('--tablename', required=True,
-	help='base name for various tables')
+parser.add_argument('--basename', required=True,
+	help='base name for various output files')
 parser.add_argument('--minexon', type=int, default=20,
 	help='minimum exon length for table3 [%(default)i]')
 parser.add_argument('--experimental', action='store_true',
-	help='genes are simulated with variable size middle exon and not coding')
+	help='special processing for the synthetic genome experiment')
 parser.add_argument('--debug', action='store_true')
 parser.add_argument('--verbose', action='store_true')
 arg = parser.parse_args()
@@ -37,25 +37,35 @@ while True:
 		if ref not in data: data[ref] = {}
 		data[ref][prog] = ali
 
+dfp = open(f'{arg.basename}_details.txt', 'w') # for detailed output
+
 # Single exon
 
 d1 = {}
 for rstr in data:
 	ref = FTX.parse(rstr)
 	if len(ref.exons) != 1: continue
+	print(ref, file=dfp)
 	for prog, astr in data[rstr].items():
-		if astr == 'None': r = 'unaligned'
+		if astr == 'None':
+			r = 'unaligned'
+			ali = 'None'
 		else:
 			ali = FTX.parse(astr)
-			if ref.beg == ali.beg and ref.end == ali.end: r = 'match'
-			else: r = 'artefact'
+			if ref.beg <= ali.beg and ref.end >= ali.beg:
+
+				# put artefact back in
+				if ref.beg == ali.beg and ref.end == ali.end: r = 'hit'
+				else: r = 'near'
+			else: r = 'miss'
+		print('\t', ali, r, file=dfp)
 		if prog not in d1: d1[prog] = {}
 		if r not in d1[prog]: d1[prog][r] = 0
 		d1[prog][r] += 1
 
-categories = ('match', 'unaliged', 'artefact')
+categories = ('hit', 'miss', 'near', 'unaliged')
 
-with open(f'{arg.tablename}_table1.tsv', 'w') as fp:
+with open(f'{arg.basename}_table1.tsv', 'w') as fp:
 	print('program\t', '\t'.join(categories), file=fp)
 	for prog in d1:
 		print(prog, end='\t', file=fp)
@@ -65,32 +75,36 @@ with open(f'{arg.tablename}_table1.tsv', 'w') as fp:
 		print(file=fp)
 
 # Two exons
-# To do: check strands, check canonical
+# Experimental: segment by site
 
 d2 = {}
 for rstr in data:
 	ref = FTX.parse(rstr)
 	if len(ref.exons) != 2: continue
+	print(ref, file=dfp)
 	emin = min(ref.exon_length(0), ref.exon_length(1))
 	for prog, astr in data[rstr].items():
-		if astr == 'None': r = 'unaligned'
+		if astr == 'None':
+			r = 'unaligned'
+			ali = 'None'
 		else:
 			ali = FTX.parse(astr)
-			if len(ali.exons) == 1: r = 'unspliced'
-			elif len(ali.exons) > 2: r = 'artefact'
+			if len(ali.exons) == 1: r = 'under'
+			elif len(ali.exons) > 2: r = 'over'
 			else:
 				score = ref.compare_introns(ali)
-				if   score == 2: r = 'match'
+				if   score == 2: r = 'correct'
 				elif score == 1: r = 'partial'
-				else:            r = 'artefact'
+				else:            r = 'wrong'
+		print('\t', ali, r, file=dfp)
 		if emin not in d2: d2[emin] = {}
 		if prog not in d2[emin]: d2[emin][prog] = {}
 		if r not in d2[emin][prog]: d2[emin][prog][r] = 0
 		d2[emin][prog][r] += 1
 
-categories = ('match', 'partial', 'unspliced', 'unaligned', 'artefact')
+categories = ('correct', 'partial', 'over', 'under', 'unaligned', 'wrong')
 
-with open(f'{arg.tablename}_table2.tsv', 'w') as fp:
+with open(f'{arg.basename}_table2.tsv', 'w') as fp:
 	print('length\tprogram\t', '\t'.join(categories), file=fp)
 	for length in sorted(d2):
 		for prog in sorted(progs):
@@ -101,7 +115,7 @@ with open(f'{arg.tablename}_table2.tsv', 'w') as fp:
 			print(file=fp)
 
 # Three Exons
-# To do: focus on middle exon
+# Experimental: focus on middle exon
 
 d3 = {}
 for rstr in data:
@@ -109,30 +123,33 @@ for rstr in data:
 	if len(ref.exons) != 3: continue
 	if ref.exon_length(0) < arg.minexon: continue
 	if ref.exon_length(2) < arg.minexon: continue
+
+	print(ref, file=dfp)
 	emin = ref.exon_length(1)
 
 	for prog, astr in data[rstr].items():
-		if astr == 'None': r = 'unaligned'
+		if astr == 'None':
+			r = 'unaligned'
+			ali = 'None'
 		else:
 			ali = FTX.parse(astr)
 			if len(ali.exons) == 1: r = 'unspliced'
-			elif len(ali.exons) > 3: r = 'artefact'
 			else:
 				score = ref.compare_introns(ali)
-				if   score == 4: r = 'match'
-				elif score == 3: r = 'partial'
-				elif score == 2: r = 'partial'
-				elif score == 1: r = 'partial'
+				if   score == 4: r = 'correct'
+				elif score == 3: r = 'found3'
+				elif score == 2: r = 'found2'
+				elif score == 1: r = 'found1'
 				else:            r = 'artefact'
-
+		print('\t', ali, r, file=dfp)
 		if emin not in d3: d3[emin] = {}
 		if prog not in d3[emin]: d3[emin][prog] = {}
 		if r not in d3[emin][prog]: d3[emin][prog][r] = 0
 		d3[emin][prog][r] += 1
 
-categories = ('match', 'partial', 'unspliced', 'unaligned', 'artefact')
+categories = ('correct', 'found1', 'found2', 'found3', 'unaligned', 'artefact')
 
-with open(f'{arg.tablename}_table3.tsv', 'w') as fp:
+with open(f'{arg.basename}_table3.tsv', 'w') as fp:
 	print('length\tprogram\t', '\t'.join(categories), file=fp)
 	for length in sorted(d3):
 		for prog in sorted(progs):
@@ -141,3 +158,9 @@ with open(f'{arg.tablename}_table3.tsv', 'w') as fp:
 				if r not in d3[length][prog]: print('0', end='\t', file=fp)
 				else: print(d3[length][prog][r], end='\t', file=fp)
 			print(file=fp)
+
+# Four Exons
+# This doesn't happen in the synthetic data
+# In real data, we are ignoring this for now
+
+dfp.close()
