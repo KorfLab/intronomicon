175b07ca43c66c85e6b467c536e12f78a7b90171
Ian Korf <ifkorf@ucdavis.edu>
2025-02-16 11:31:32 -0800

    up

--- downloader.py
+++ downloader.py
@@ -11,6 +11,26 @@ import time
 
 from ncbi_reader import sraxml_read, soft_read
 
+def remote_filesize(url, arg):
+	try:
+		if arg.verbose: print(f'checking filesize at {url}')
+		text = run(f'wget --spider "{url}"', check=True, shell=True,
+			capture_output=True).stderr.decode()
+	except:
+		print(f'remote_filesize wget failed on {url}', file=sys.stderr)
+		return None
+		
+	m = re.search(r'Length: (\d+) ', text)
+	if not m:
+		print(f'remote_filesize grep failed on {text}', file=sys.stderr)
+		return None
+		
+	size = int(m.group(1))
+	if arg.verbose: print(f'size: {size} bytes')	
+	
+	time.sleep(arg.delay)
+	return size
+
 def retrieve(url, arg):
 	for _ in range(arg.retry + 1):
 		if arg.verbose: print(f'requesting: {url}', file=sys.stderr)
@@ -34,8 +54,9 @@ def should_be_one(things):
 	if len(things) == 0: sys.exit('should have existed')
 	sys.exit('more than one thing found')
 
-
-###########################################################################
+##############################################################################
+# CLI
+##############################################################################
 
 parser = argparse.ArgumentParser(description='Get GEO & SRA files from NCBI')
 parser.add_argument('dir', help='output directory (e.g. build)')
@@ -45,6 +66,10 @@ parser.add_argument('--delay', type=float, default=0.5,
 	help='delay between download requests [%(default).2f]')
 parser.add_argument('--retry', type=int, default=5,
 	help='number of times to retry after download failure [%(default)i]')
+parser.add_argument('--max-gse-size', type=float, default=1e7,
+	help='maximum size of gse.soft file [%(default).0f]')
+parser.add_argument('--max-supp-size', type=float, default=1e8,
+	help='maximum size of supplementary file [%(default).0f]')
 parser.add_argument('--verbose', action='store_true',
 	help='print status messages')
 parser.add_argument('--debug', action='store_true')
@@ -54,7 +79,7 @@ subs = ('gds_txt', 'gse_soft', 'gse_supp', 'gsm_soft', 'sra_xml')
 for sub in subs: os.system(f'mkdir -p {arg.dir}/{sub}')
 
 ##############################################################################
-# PART 1: Retrieve GEO data for taxid
+# PART 0: Retrieve all GEO data series txt files for specific taxid
 ##############################################################################
 
 base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
@@ -69,9 +94,7 @@ n = int(re.search(r'<Count>(\d+)</Count>', txt).group(1))
 url = f'{base}/esearch.fcgi?db=gds&{taxid}&retmax={n}'
 txt = retrieve(url, arg)
 
-# download each GSE family soft file
-done = 0
-failures = []
+# download all gds text files
 for m in re.finditer(r'<Id>(\d+)</Id>', txt):
 
 	# get the uid for each gds record that match (2nnnnnnnn)
@@ -82,39 +105,55 @@ for m in re.finditer(r'<Id>(\d+)</Id>', txt):
 	if os.path.exists(gds_file):
 		if arg.verbose: print('skipping', gds_file)
 		continue
-	
-	# get the ftp location for the GSE from the gds record
-	url = f'{base}/efetch.fcgi?db=gds&id={uid}'
 	gds_text = retrieve(url, arg)
-	m = re.search(r'(ftp:\S+)', gds_text)
-	if not m: # no ftp file listed
-		failures.append(('no-ftp-file', url))
+	with open(gds_file, 'w') as fp: print(gds_text, file=fp)
+	
+	if arg.debug:
+		print('stopping to debug')
+		break
+	
+##############################################################################
+# PART 1: Retrieve all GEO series soft files
+##############################################################################
+
+for filename in glob.glob(f'{arg.dir}/gds_txt/*'):
+	with open(filename) as fp: gds_text = fp.read()
+	m = re.search(r'ftp:..(\S+)/', gds_text)
+	if not m:
+		if arg.verbose: print(f'no ftp in {filename}')
 		continue
+	
 	ftp_url = m.group(1)
 	m = re.search(r'n\/(GSE\d+)', gds_text)
-	if not m: # no GSE file listed
-		failures.append(('ftp-fail', ftp_url))
-		continue
-	
-	# fetch the gse soft file
 	gse_id = m.group(1)
-	soft_url = f'{ftp_url}soft/{gse_id}_family.soft.gz'
+	soft_url = f'{ftp_url}/soft/{gse_id}_family.soft.gz'
 	gse_file = f'{arg.dir}/gse_soft/{gse_id}_family.soft.gz'
-	try:
-		run(f'wget -P {arg.dir}/gse_soft "{soft_url}"', check=True, shell=True)
-		if os.path.getsize(gse_file) > 100000: # has embedded microarray shit
-			os.unlink(gse_file)
-			os.system(f'touch {gse_file}') # pretend we have it
-	except: # wget fails (URL has no soft file)
-		failures.append(('soft-fail', soft_url))
+	
+	if os.path.exists(gse_file):
+		if arg.verbose: print('skipping', gse_file)
 		continue
 	
-	# save the gds
-	with open(gds_file, 'w') as fp: print(gds_text, file=fp)	
-	if arg.debug: break
+	size = remote_filesize(soft_url, arg)
+	if size > arg.max_gse_size:
+		if arg.verbose: print(f'gse too large {size}')
+		os.system(f'touch {gse_file}')
+		continue
+	
+	try:
+		cli = f'wget -P {arg.dir}/gse_soft "{soft_url}"'
+		if arg.verbose: print(cli, flush=True)
+		run(cli, check=True, shell=True)
+		time.sleep(arg.delay)
+	except:
+		if arg.verbose: print(f'wget failed on {soft_url}')
+		continue
+		
+	if arg.debug:
+		print('stopping to debug')
+		break
 
 ##############################################################################
-# PART 2: Select the GSEs worth exploring
+# PART 2: Download GSE supps and GSMs but filter
 #  - RNA-Seq
 #  - Not mixtures of RNA-Seq with other stuff
 #  - Have supplementary gene expression files
@@ -135,12 +174,10 @@ for path in glob.glob(f'{arg.dir}/gse_soft/*'):
 	if 'high-throughput sequencing' not in techs: continue
 	if len(taxids) > 1: continue
 	if arg.taxid not in taxids: sys.exit(f'taxid mismatch')
-
+	
 	# skip anything that isn't normal RNA-Seq
 	srcs = set()
 	strats = set()
-	bios = []
-	sras = []
 	weird = False
 	for k, sdata in d.items():
 		if not k.startswith('SAMPLE'): continue
@@ -149,58 +186,49 @@ for path in glob.glob(f'{arg.dir}/gse_soft/*'):
 		strat = should_be_one(sdata['Sample_library_strategy'])
 		srcs.add(src)
 		strats.add(strat)
-
-		b = []
-		s = []
-		for text in sdata['Sample_relation']:
-			if text.startswith('BioSample'): b.append(text[10:-1])
-			if text.startswith('SRA'): s.append(text[4:-1])
-		
-		if len(b) == 1: bios.append(b[0])
-		else: weird = True
-		if len(s) == 1: sras.append(s[0])
-		else: weird = True
-	
-	if weird: continue
-	if len(srcs) != 1 or len(strats) != 1: continue # multi-omic something
 	if 'transcriptomic' not in srcs: continue
 	if 'RNA-Seq' not in strats: continue
-	if len(bios) != len(sras): sys.exit('wtf')
-	
+
 	# find files that may be gene expression counts
 	not_wanted = ('RAW.tar', '.bw', '.bigwig', '.pdf', 'pdf.gz', '.txt',
 		'.bedgraph.gz', '.fasta.gz', '.wig.gz')
 	keep = []
 	skey = findkeys(d, 'SERIES')[0]
-	if 'Series_supplementary_file' not in d[skey]: continue
+	if 'Series_supplementary_file' not in d[skey]:
+		if arg.verbose: print(f'no supps in {path}')
+		continue
+		
 	for filename in d[skey]['Series_supplementary_file']:
 		skip = False
 		for ext in not_wanted:
 			if filename.endswith(ext):
 				skip = True
 				break
-		if skip: continue
+		if skip: continue		
 		keep.append(filename)
 	if len(keep) == 0: continue
 	
+	# download supps
 	gse_id = skey.split()[2]
 	gse_dir = f'{arg.dir}/gse_supp/{gse_id}'
 	os.system(f'mkdir -p {gse_dir}')
-	
 	for url in keep:
 		filename = url.split('/')[-1]
 		filepath = f'{gse_dir}/{filename}'
+		
 		if os.path.exists(filepath):
 			if arg.verbose: print('skipping', filepath)
 			continue
-		try:
-			run(f'wget -P {gse_dir} "{url}"', check=True, shell=True)
-			time.sleep(0.5) # change to arg
-		except:
-			gse_fails.append(url)
+
+		size = remote_filesize(url, arg)
+		if size is None or size > arg.max_supp_size:
+			os.system(f'touch {filepath}') # don't attempt download again
 			continue
+			
+		run(f'wget -P {gse_dir} "{url}"', check=True, shell=True)
+		time.sleep(arg.delay)
 		
-	# retrieve GSMs (just the good ones, is this even necessary?)
+	# retrieve GSMs
 	for k, sdata in d.items():
 		if not k.startswith('SAMPLE'): continue
 		gsm_id = should_be_one(sdata['Sample_geo_accession'])
@@ -213,12 +241,10 @@ for path in glob.glob(f'{arg.dir}/gse_soft/*'):
 			query = f'{gsm_id}&targ=self&view=brief&form=text'
 			gsm_txt = retrieve(f'{base}{query}', arg)
 			with open(gsm_file, 'w') as fp: fp.write(gsm_txt)
-	
 
 ##############################################################################
 # PART 3: Download XML from SRA
 ##############################################################################
-
 
 base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
 taxid = f'term=txid{arg.taxid}[Organism]'
